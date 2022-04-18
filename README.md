94da8262cf90cbe5ac3007c9abf6494e486109f9
Sirius-Star42 <sirius.star4242@gmail.com>
2022-04-18 22:46:18 +0300

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
  
+# Node.js- Socket.io
+
+This project is prepared to run socket listens on node.js
+
+*Please feel free to use..*
+
+
+## Authors
+
+- [@sirius.star](https://www.linkedin.com/in/sirius-star)
+
+
+## Tech Stack
+
+**Client:** Pure HTML
+
+**Server:** Node, Express
+
+
+## Environment Variables
+
+To run this project, you will need to install the following environment
+
+`socket.io`
+
+`epress`
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Sirius-Star42/nodejs_chat_app.git
+
+Go to the project directory
+
+```bash
+  cd main folder
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run server
+```
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://sirius-star42.github.io/Hasan-DALKILIC/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/sirius-star)
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Support
+
+For support, email sirius.star4242@gmail.com or request connect to linkedin.
+
--- public/chat.js
+++ public/chat.js
@@ -1,18 +1,29 @@
-const socket = io.connect('http://localhost:3000')
+const socket = io.connect("http://localhost:3000");
 
-const sender = document.getElementById('sender')
-const message = document.getElementById('message')
-const submitBtn = document.getElementById('submitBtn')
-const output = document.getElementById('output')
-const feedback = document.getElementById('feedback')
+const sender = document.getElementById("sender");
+const message = document.getElementById("message");
+const submitBtn = document.getElementById("submitBtn");
+const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
 
-submitBtn.addEventListener('click', () => {
-    socket.emit('chat', {
-        message: message.value,
-        sender: sender.value
-    })
-})
+submitBtn.addEventListener("click", () => {
+  socket.emit("chat", {
+    message: message.value,
+    sender: sender.value,
+  });
+});
 
-socket.on('chat', data => {
-    output.innerHTML += '<p><strong> ' + data.sender + '</strong>' + data.message + '</p>'
-})
+socket.on("chat", (data) => {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.sender + "</strong>: " + data.message + "</p>";
+  message.value = "";
+});
+
+message.addEventListener("keypress", () => {
+  socket.emit("typing", sender.value);
+});
+
+socket.on("typing", (data) => {
+  feedback.innerHTML = "<p><i>" + data + " typing</i>...</p>";
+});
